b1fa74b4b290a73d2161e378db3e9450a91bd510
Sappy <sappy5678@gmail.com>
2024-12-14 15:21:39 +0800

    remove postgresql orm, using sqlx

--- pkg/utl/postgres/pg.go
+++ pkg/utl/postgres/pg.go
@@ -1,49 +1,21 @@
 package postgres
 
 import (
-	"context"
-	"fmt"
-	"time"
+	"github.com/jmoiron/sqlx"
 
-	"github.com/go-pg/pg/v9"
 	// DB adapter
 	_ "github.com/lib/pq"
 )
 
-type dbLogger struct{}
-
-// BeforeQuery hooks before pg queries
-func (d dbLogger) BeforeQuery(c context.Context, q *pg.QueryEvent) (context.Context, error) {
-	return c, nil
-}
-
-// AfterQuery hooks after pg queries
-func (d dbLogger) AfterQuery(c context.Context, q *pg.QueryEvent) error {
-	query, err := q.FormattedQuery()
-	fmt.Println(query)
-	return err
-}
-
 // New creates new database connection to a postgres database
-func New(psn string, timeout int, enableLog bool) (*pg.DB, error) {
-	u, err := pg.ParseURL(psn)
+func New(psn string) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("postgres", psn)
 	if err != nil {
 		return nil, err
 	}
 
-	db := pg.Connect(u)
-
-	_, err = db.Exec("SELECT 1")
-	if err != nil {
+	if db.Ping() != nil {
 		return nil, err
-	}
-
-	if timeout > 0 {
-		db = db.WithTimeout(time.Second * time.Duration(timeout))
-	}
-
-	if enableLog {
-		db.AddQueryHook(dbLogger{})
 	}
 
 	return db, nil
